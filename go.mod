b6598081d6dd5e3443b5f507af2af54fa438af0e
Aleksey <alekssum@gmail.com>
2020-06-20 14:08:40 +0300

    part 3

--- go.mod
+++ go.mod
@@ -4,9 +4,12 @@ go 1.13
 
 require (
 	github.com/BurntSushi/toml v0.3.1
+	github.com/go-ozzo/ozzo-validation v3.6.0+incompatible
+	github.com/go-ozzo/ozzo-validation/v4 v4.2.1 // indirect
 	github.com/gorilla/mux v1.7.4
 	github.com/lib/pq v1.7.0
 	github.com/onsi/ginkgo v1.13.0 // indirect
 	github.com/sirupsen/logrus v1.6.0
-	github.com/stretchr/testify v1.3.0
+	github.com/stretchr/testify v1.4.0
+	golang.org/x/crypto v0.0.0-20200604202706-70a84ac30bf9
 )
--- internal/store/userrepository_test.go
+++ internal/store/userrepository_test.go
@@ -1,7 +1,6 @@
 package store_test
 
 import (
-	"log"
 	"testing"
 
 	"github.com/alekssum/http-rest-api/internal/app/model"
@@ -13,10 +12,7 @@ func TestUserRepository_Create(t *testing.T) {
 	s, teardown := store.TestStore(t, databaseURL)
 	defer teardown("users")
 
-	u, err := s.User().Create(&model.User{
-		Email: "user@example.org",
-	})
-
+	u, err := s.User().Create(model.TestUser(t))
 	assert.NoError(t, err)
 	assert.NotNil(t, u)
 }
@@ -30,12 +26,11 @@ func TestUserRepository_FindByEmail(t *testing.T) {
 	_, err := s.User().FindByEmail(email)
 	assert.Error(t, err)
 
-	s.User().Create(&model.User{
-		Email: email,
-	})
+	u := model.TestUser(t)
+	u.Email = email
+	s.User().Create(u)
 
-	u, err := s.User().FindByEmail(email)
-	log.Println("user:", u)
+	u, err = s.User().FindByEmail(email)
 	assert.NoError(t, err)
 	assert.NotNil(t, u)
 }
